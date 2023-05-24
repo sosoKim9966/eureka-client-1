1a80756fa0a0645d51e260e67d83518f69ccc8d9
sosoKim9966 <sosokim9966@gmail.com>
2023-05-24 22:36:02 +0900

    post, user

---
+++ src/main/java/com/example/eureka/client/eurekaclient1/domain/Post.java
@@ -0,0 +1,44 @@
+package com.example.eureka.client.eurekaclient1.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false,columnDefinition = "TEXT")
+    private String content;
+
+    private String email;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_no")
+    private User user;
+
+    @Builder
+    public Post(String title, String content, String email, User user){
+        this.title = title;
+        this.content = content;
+        this.email = email;
+        this.user = user;
+    }
+
+    public void updatePost(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+}
---
+++ src/main/java/com/example/eureka/client/eurekaclient1/domain/User.java
@@ -0,0 +1,31 @@
+package com.example.eureka.client.eurekaclient1.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_no")
+    private Long id;
+
+    private String email;
+    private String userName;
+    private String password;
+
+    @Builder
+    public User(String email, String userName, String password) {
+        this.email = email;
+        this.userName = userName;
+        this.password = password;
+    }
+
+}
